0183b8e75aa6a2994775eee34597821807e3cb37
Huxlyx <mtengler@gmail.com>
2024-12-05 16:05:37 +0100

    Day 5

---
+++ Src/Day05.cs
@@ -0,0 +1,104 @@
+﻿namespace AdventOfCode2024.Src;
+
+internal class Day05 : IAoC
+{
+    public void Part1(IAoC aoc)
+    {
+        string[] lines = aoc.GetContent();
+
+        List<(int, int)> pageRules = [];
+
+        int lineIdx = 0;
+        foreach (string line in lines)
+        {
+            ++lineIdx;
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                break;
+            }
+            int[] pages = line.Split('|').Select(int.Parse).ToArray();
+            pageRules.Add((pages[0], pages[1]));
+        }
+
+        int result = 0;
+
+        for (int i = lineIdx; i < lines.Length; i++)
+        {
+            string currentLine = lines[i];
+            int[] nums = currentLine.Split(',').Select(int.Parse).ToArray();
+
+            bool broken = false;
+
+            for (int firstPage = 0; firstPage < nums.Length - 1 && ! broken; ++firstPage)
+            {
+                for (int secondPage = firstPage + 1; secondPage < nums.Length && ! broken; ++secondPage)
+                {
+                    foreach ((int first, int second) in pageRules)
+                    {
+                        if (nums[firstPage] == second && nums[secondPage] == first)
+                        {
+                            broken = true;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            if ( ! broken)
+            {
+                result += nums[nums.Length >> 1];
+            }
+        }
+
+        Console.WriteLine(result);
+    }
+
+    public void Part2(IAoC aoc)
+    {
+        string[] lines = aoc.GetContent();
+
+        List<(int, int)> pageRules = [];
+
+        int lineIdx = 0;
+        foreach (string line in lines)
+        {
+            ++lineIdx;
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                break;
+            }
+            int[] pages = line.Split('|').Select(int.Parse).ToArray();
+            pageRules.Add((pages[0], pages[1]));
+        }
+
+        int result = 0;
+
+        for (int i = lineIdx; i < lines.Length; i++)
+        {
+            string currentLine = lines[i];
+            int[] nums = currentLine.Split(',').Select(int.Parse).ToArray();
+
+            bool broken = false;
+            for (int firstPage = 0; firstPage < nums.Length - 1; ++firstPage)
+            {
+                for (int secondPage = firstPage + 1; secondPage < nums.Length; ++secondPage)
+                {
+                    foreach ((int first, int second) in pageRules)
+                    {
+                        if (nums[firstPage] == second && nums[secondPage] == first)
+                        {
+                            (nums[secondPage], nums[firstPage]) = (nums[firstPage], nums[secondPage]);
+                            broken = true;
+                        }
+                    }
+                }
+            }
+            if (broken)
+            {
+                result += nums[nums.Length >> 1];
+            }
+        }
+
+        Console.WriteLine(result);
+    }
+}
